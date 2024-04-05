5a2030ea597dbb1983257a0b1d8cafa509100e48
Raj Nandan Sharma <raj@cashfree.com>
2024-04-05 20:45:02 +0530

    release

--- cmd/root.go
+++ cmd/root.go
@@ -17,6 +17,7 @@ var rootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Version: "0.0.1",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
