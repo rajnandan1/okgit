57196d237f229a734cb50dbcadf38226f0a74705
Raj Nandan Sharma <raj@CFs-MacBook-Pro-3.local>
2024-05-29 21:26:01 +0530

    automated release

--- cmd/root.go
+++ cmd/root.go
@@ -17,7 +17,7 @@ var rootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
-	Version: "1.0.14",
+	Version: "1.0.15",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
