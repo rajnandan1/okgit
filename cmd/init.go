e305a62ce0a548dcb5c4be68cf61865e1456bc0b
Raj Nandan Sharma <raj@CFs-MacBook-Pro-3.local>
2024-05-29 20:46:13 +0530

    fix: added okgit to root folder

--- cmd/init.go
+++ cmd/init.go
@@ -1,42 +1,20 @@
 package cmd
 
 import (
-	"io/ioutil"
-	"path/filepath"
-	"strings"
-
 	"github.com/rajnandan1/okgit/utils"
 	"github.com/spf13/cobra"
 )
 
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "Add .okgit/ to the .gitignore file",
+	Short: "Add .okgit/ to home folder",
 	Run: func(cmd *cobra.Command, args []string) {
-		gitignorePath := filepath.Join(".", ".gitignore")
-		okgitPath := ".okgit/"
-
-		// Read the contents of the .gitignore file
-		data, err := ioutil.ReadFile(gitignorePath)
+		_, err := utils.StartOkgit()
 		if err != nil {
 			utils.LogFatal(err)
 		}
+		utils.LogOutput(".okgit/ added to home folder")
 
-		// Append .okgit/ to the .gitignore file if it's not already present
-		contents := string(data)
-		if !strings.Contains(contents, okgitPath) {
-			contents += okgitPath + "\n"
-
-			// Write the updated contents back to the .gitignore file
-			err = ioutil.WriteFile(gitignorePath, []byte(contents), 0644)
-			if err != nil {
-				utils.LogFatal(err)
-			}
-
-			utils.LogOutput(".okgit/ added to .gitignore file")
-		} else {
-			utils.LogOutput(".okgit/ already present in .gitignore file")
-		}
 	},
 }
 
